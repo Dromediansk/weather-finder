c43c4d21b6a94fbac675df3469c28f84534b553f
Dromediansk <pillar.mr@gmail.com>
2019-01-17 16:32:32 +0100

    Updates

---
+++ precache-manifest.8d55da13370374822d1da55899b84500.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-finder/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "f4ddb555f7ed442db414",
+    "url": "/weather-finder/static/js/runtime~main.f4ddb555.js"
+  },
+  {
+    "revision": "10540abf4b235eceaf8a",
+    "url": "/weather-finder/static/js/main.10540abf.chunk.js"
+  },
+  {
+    "revision": "60e67b10abb24248c60b",
+    "url": "/weather-finder/static/js/3.60e67b10.chunk.js"
+  },
+  {
+    "revision": "45975131c9cf17803e28",
+    "url": "/weather-finder/static/js/2.45975131.chunk.js"
+  },
+  {
+    "revision": "bed51d6f980116aa6250",
+    "url": "/weather-finder/static/js/1.bed51d6f.chunk.js"
+  },
+  {
+    "revision": "10540abf4b235eceaf8a",
+    "url": "/weather-finder/static/css/main.86a1af39.chunk.css"
+  },
+  {
+    "revision": "980f9fd9a28c6194102495cc0a09cbe8",
+    "url": "/weather-finder/index.html"
+  }
+];
